02c1b5e4fcb2bf67b248078e252dabacf8baaea2
Ekokurniadi <ekokurniadi.02@gmail.com>
2022-01-22 10:15:10 +0700

    tipe data number

--- build/typescript-dasar.js
+++ build/typescript-dasar.js
@@ -2,3 +2,6 @@
 console.log('hello');
 let nama = "Eko Kurniadi";
 nama = "Eko Kurniadi new";
+// tipe data numbe
+let umur = 26;
+umur = 5;
--- src/typescript-dasar.ts
+++ src/typescript-dasar.ts
@@ -2,3 +2,10 @@ console.log('hello')
 let nama :string = "Eko Kurniadi"
 
 nama="Eko Kurniadi new"
+
+// tipe data numbe
+
+let umur:number = 26
+
+umur =5
+
