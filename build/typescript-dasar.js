f5d3dabe7d46d7ad443d9eb8a32966d86a410f95
Ekokurniadi <ekokurniadi.02@gmail.com>
2022-01-22 09:29:15 +0700

    belajar type data string

---
+++ build/typescript-dasar.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('hello');
+let nama = "Eko Kurniadi";
+nama = "Eko Kurniadi new";
---
+++ src/typescript-dasar.ts
@@ -0,0 +1,4 @@
+console.log('hello')
+let nama :string = "Eko Kurniadi"
+
+nama="Eko Kurniadi new"
